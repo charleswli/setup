afd6d08589dfc3f328ce076972d1503b40e49c88
Charles Li <charlesweiyili@gmail.com>
2013-07-10 22:26:37 -0400

    initial commit

---
+++ django.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y emacs
+sudo apt-get install -y git
+sudo apt-get install -y python-pip
+sudo apt-get install -y apache2
+sudo apt-get install -y python-mysqldb
+sudo apt-get install -y mysql-server
+
+sudo apt-get install -y libapache2-mod-wsgi
+
+sudo pip install Django
+sudo pip install requests
